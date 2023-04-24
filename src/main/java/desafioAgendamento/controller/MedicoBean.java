abacd40b946603db3b8c8381d564227fbd9e4577
Rafael Freesz <rafaelfreesz@gmail.com>
2023-04-24 19:46:40 -0300

    Listando Médicos em tabela de dados

--- src/main/java/desafioAgendamento/controller/MedicoBean.java
+++ src/main/java/desafioAgendamento/controller/MedicoBean.java
@@ -1,13 +1,16 @@
 package desafioAgendamento.controller;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 import desafioAgendamento.model.Medico;
 import desafioAgendamento.model.enums.Especializacao;
 import desafioAgendamento.model.enums.Sexo;
+import desafioAgendamento.repository.MedicoRepository;
 
 @Named
 @ViewScoped
@@ -15,14 +18,18 @@ public class MedicoBean implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	
-	private Medico medico=new Medico();
+	@Inject
+	private MedicoRepository repository;
+
+	private List<Medico> listaMedicos;
 	
-	public void salvar() {
-		System.out.println(medico);
+	
+	public void buscarTodos() {
+		listaMedicos= repository.buscarTodos();
 	}
 	
-	public Medico getMedico() {
-		return medico;
+	public List<Medico> getListaMedicos() {
+		return listaMedicos;
 	}
 	
 	public Sexo[] getSexos() {
